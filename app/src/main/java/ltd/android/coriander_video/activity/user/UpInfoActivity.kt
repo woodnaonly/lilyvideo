66fa7a7cbbd01184236b57ade42c8d88cf6d043d
asdkjlljkcxzlk <x>
2021-07-01 17:39:37 +0800

    1

---
+++ app/src/main/java/ltd/android/coriander_video/activity/user/UpInfoActivity.kt
@@ -0,0 +1,23 @@
+package ltd.android.coriander_video.activity.user
+
+import ltd.android.coriander_video.R
+import ltd.android.coriander_video.activity.base.BaseActivity
+import ltd.android.coriander_video.view_model.base.BaseViewModel
+
+//class MainActivity : AppCompatActivity(),CoroutineScope {
+class UpInfoActivity : BaseActivity<BaseViewModel>() {
+    override fun layoutId(): Int {
+        return R.layout.activity_main
+    }
+
+
+    override fun initView() {
+        super.initView()
+
+
+    }
+
+
+}
+
+
